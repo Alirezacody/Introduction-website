6eb59aa8dea20af5990b7a4fc4d6339a0c78d1ae
Alireza <nabikhanialireza738@gmail.com>
2024-03-19 18:27:21 +0330

    added command  javascript to  project

--- my.js
+++ my.js
@@ -1,1 +1,15 @@
 //Alireza
+let isprime =function(number){
+    let count=0;
+    for(let i=0;i<number;i++){
+        if(number%i==0){
+            count++
+        }
+    }
+    if(count==2){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
